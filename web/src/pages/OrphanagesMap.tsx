8fc07ec054c46feca9453bd24eed0c08100b7033
bruno-wolf <bruno.m.lobato@gmail.com>
2020-10-12 23:39:00 +0100

    feat: add leaflet map to orphanages-map page

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import mapMaker from '../images/map-marker.svg';
 import { Link } from 'react-router-dom';
-import{ FiPlus } from 'react-icons/fi';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 import '../styles/pages/orphanages-map.css';
 
 function OrphanagesMap(){
@@ -18,7 +20,11 @@ function OrphanagesMap(){
           <span>Lisboa</span>
         </footer>
       </aside>
-      <div></div>
+      
+      <Map center={[38.6963499,-9.2231394]} zoom={16} style={{width:'100%', height: '100%'}}>
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      </Map>
+
       <Link to="" className="create-orphanage">
         <FiPlus size={32} color="#fff" />
       </Link>
