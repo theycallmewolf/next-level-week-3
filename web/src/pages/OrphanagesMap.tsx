12421dc4a3cddf1c72bf15405fc6863697b49b27
bruno-wolf <bruno.m.lobato@gmail.com>
2020-10-12 23:14:06 +0100

    style: orphanages-map page

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import mapMaker from '../images/map-marker.svg';
+import { Link } from 'react-router-dom';
+import{ FiPlus } from 'react-icons/fi';
+import '../styles/pages/orphanages-map.css';
 
 function OrphanagesMap(){
   return (
-    <h1>Hello Wolf</h1>
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMaker} alt="Happy icon"/>
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+        <footer>
+          <strong>Algés</strong>
+          <span>Lisboa</span>
+        </footer>
+      </aside>
+      <div></div>
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#fff" />
+      </Link>
+    </div>
   )
 }
 
