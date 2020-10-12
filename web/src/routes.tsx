76a347ddf09867b0500e8e7362ccf256af9c9466
bruno-wolf <bruno.m.lobato@gmail.com>
2020-10-12 22:25:48 +0100

    feat: added routes file with react-router-dom

---
+++ web/src/routes.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+function Routes(){
+  return(
+    <BrowserRouter>
+      <Route path="/" component={} />
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
