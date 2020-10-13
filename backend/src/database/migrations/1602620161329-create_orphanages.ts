0de960985cb95c42169fe929a4b8eeee0879ef6d
bruno-wolf <bruno.m.lobato@gmail.com>
2020-10-13 22:14:03 +0100

    add first item to orphanage table

--- backend/src/database/migrations/1602620161329-create_orphanages.ts
+++ backend/src/database/migrations/1602620161329-create_orphanages.ts
@@ -41,6 +41,10 @@ export class createOrphanages1602620161329 implements MigrationInterface {
               type: 'text',
             },
             {
+              name: 'opening_hours',
+              type: 'varchar'
+            },
+            {
               name: 'open_on_weekends',
               type: 'boolean',
               default: false
