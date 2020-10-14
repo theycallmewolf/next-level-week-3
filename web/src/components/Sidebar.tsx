093b4a1de091019991901e5231bed2e0c48a537e
bruno-wolf <bruno.m.lobato@gmail.com>
2020-10-14 19:53:47 +0100

    added mapIcon util

--- web/src/components/Sidebar.tsx
+++ web/src/components/Sidebar.tsx
@@ -4,7 +4,6 @@ import mapMarkerImg from '../images/map-marker.svg';
 import { FiArrowLeft } from "react-icons/fi";
 import '../styles/components/Sidebar.css';
 
-
 export default function Sidebar(){
 
   const { goBack } = useHistory();
@@ -12,7 +11,6 @@ export default function Sidebar(){
   return(
     <aside className="app-sidebar">
       <img src={mapMarkerImg} alt="Happy" />
-
       <footer>
         <button type="button" onClick={ goBack }>
           <FiArrowLeft size={24} color="#FFF" />
