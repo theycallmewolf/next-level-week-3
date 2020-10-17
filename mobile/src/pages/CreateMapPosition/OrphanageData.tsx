eb8808098b53cf624076c2b07715804cb800d374
bruno-wolf <bruno.m.lobato@gmail.com>
2020-10-17 13:37:02 +0100

    feat: send new orphanage data to server

--- mobile/src/pages/CreateMapPosition/OrphanageData.tsx
+++ mobile/src/pages/CreateMapPosition/OrphanageData.tsx
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import { ScrollView, View, StyleSheet, Switch, Text, TextInput, TouchableOpacity, Image } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { RectButton } from 'react-native-gesture-handler';
-import { useRoute } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import * as ImagePicker from 'expo-image-picker';
+import api from '../../services/api';
 
 interface OrphanageDataRouteParams {
   position: {
@@ -22,23 +23,46 @@ export default function OrphanageData() {
   const [open_on_weekends, setOpenOnWeekends] = useState(true);
   const [images, setImages] = useState<string[]>([]);
 
-  console.log(route.params);
+  const navigation = useNavigation();
+  // console.log(route.params);
 
   const params = route.params as OrphanageDataRouteParams;
 
-  function handleCreateOrphanage(){
+  async function handleCreateOrphanage(){
 
     const { latitude, longitude } = params.position;
 
-    console.log({
-      name,
-      about,
-      instructions,
-      opening_hours,
-      open_on_weekends,
-      latitude,
-      longitude
+    // console.log({
+    //   name,
+    //   about,
+    //   instructions,
+    //   opening_hours,
+    //   open_on_weekends,
+    //   latitude,
+    //   longitude
+    // })
+
+    const data = new FormData();
+
+    data.append('name', name);
+    data.append('about', about);
+    data.append('latitude', String(latitude));  // FormData only handles strings
+    data.append('longitude', String(longitude));
+    data.append('instructions', instructions);
+    data.append('opening_hours', opening_hours);
+    data.append('open_on_weekends', String(open_on_weekends));
+
+    images.forEach((image, index) =>{
+      data.append('images', {
+        name : `image_${index}.jpg`, // error on package.json
+        type : 'image/jpg',
+        uri : image,
+      } as any) // fix dor error
     })
+
+    await api.post('orphanages', data);
+    navigation.navigate('OrphanagesMap');
+
   }
 
   async function handleSelectImages(){
@@ -55,7 +79,7 @@ export default function OrphanageData() {
       mediaTypes : ImagePicker.MediaTypeOptions.Images, // accept only images
     })
 
-    console.log(result);
+    // console.log(result);
 
     if(result.cancelled) { return; }
 
--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import MapView from 'react-native-maps';
 import { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps';
 import mapMarker from '../images/map-marker.png';
@@ -22,11 +22,11 @@ export default function OrphanagesMap(){
 
   console.log(orphanages);
 
-  useEffect(() => {
+  useFocusEffect(() => {                      //
     api.get('orphanages').then(response => {
       setOrphanages(response.data);
     });
-  }, []);
+  });
 
   function handleNavigateToOrphanageDetails(id: number){
     navigation.navigate("OrphanageDetails", { id })
